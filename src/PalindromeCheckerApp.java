e6521b3f2f3a6f3de2e7aa2200bad81798cbab3d
TriparnoXd <triparnodas16734@gmail.com>
2026-03-20 09:51:02 +0530

    uc1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
 public class PalindromeCheckerApp {
+
+    public static void main(String []args){
+        System.out.println("Welcome to Palindrome checker app");
+        System.out.println("Version 1");
+        System.out.println("System initialized successfully");
+    }
 }
